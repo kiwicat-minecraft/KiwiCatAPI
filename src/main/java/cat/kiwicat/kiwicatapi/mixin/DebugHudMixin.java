0d2267ebde63e33852c2540543014a674fdbe85f
Kiwicat <kiwicat@kiwicat.cat>
2026-06-15 19:33:06 +0200

    FIxed im sure

--- src/main/java/cat/kiwicat/kiwicatapi/mixin/DebugHudMixin.java
+++ src/main/java/cat/kiwicat/kiwicatapi/mixin/DebugHudMixin.java
@@ -1,4 +1,28 @@
 package cat.kiwicat.kiwicatapi.mixin;
 
-public class DebugHudMixin {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import cat.kiwicat.kiwicatapi.KiwiCatAPI;
+import net.minecraft.client.gui.hud.DebugHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DebugHud.class)
+public abstract class DebugHudMixin {
+    @Inject(
+            method = {"getLeftText"},
+            at = {@At("RETURN")},
+            cancellable = true
+    )
+    private void addCustomInfo(CallbackInfoReturnable<List<String>> cir) {
+        List<String> list = new ArrayList((Collection)cir.getReturnValue());
+        list.add("");
+        list.add("KiwiCatAPI " + KiwiCatAPI.VERSION);
+        cir.setReturnValue(list);
+    }
 }
+
